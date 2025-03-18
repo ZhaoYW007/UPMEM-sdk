09dd7e1a8589afa5d4a6b2a5efb1ddcb792b9888
Yiwei Zhao <zhaoyw007@163.com>
2025-03-18 13:02:47 -0400

    Update to ver-2025.1.0

--- src/backends/hw/src/commons/dpu_region_address_translation.h
+++ src/backends/hw/src/commons/dpu_region_address_translation.h
@@ -105,7 +105,7 @@ struct dpu_region_address_translation {
     bool one_read;
 
     /* Pointer to private data for each backend implementation */
-    void *privateData;
+    void *private;
 
     /* Returns -errno on error, 0 otherwise. */
     int (*init_rank)(struct dpu_region_address_translation *tr, uint8_t channel_id);
--- src/dpu-rt/Toolchain-dpu.cmake
+++ src/dpu-rt/Toolchain-dpu.cmake
@@ -5,15 +5,17 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR dpu)
 
-set(TRIPLE dpu-upmem-dpurte)
+set(triple dpu-upmem-dpurte)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
 set(CMAKE_ASM_COMPILER clang)
+set(CMAKE_ASM_COMPILER_TARGET ${triple})
 set(CMAKE_AR llvm-ar)
-set(CMAKE_C_COMPILER_TARGET ${TRIPLE})
-set(CMAKE_ASM_COMPILER_TARGET ${TRIPLE})
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
@@ -21,3 +23,13 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(CMAKE_C_COMPILER_FORCED 1)
+
+set(CHIP_VERSION "v1A")  # Default value
+if(EXISTS "/sys/class/dpu_rank/dpu_rank0/dpu_chip_id")
+    file(READ "/sys/class/dpu_rank/dpu_rank0/dpu_chip_id" CHIP_ID_NUMBER)
+    if(CHIP_ID_NUMBER GREATER 8)
+        set(CHIP_VERSION "v1B")
+    endif()
+endif()
+
+set(CMAKE_C_FLAGS_INIT "-mcpu=${CHIP_VERSION} -g")
